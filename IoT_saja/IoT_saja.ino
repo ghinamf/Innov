42e7d23657ab696ae854cd8bd511b97352508b27
ghinamf <mufidahg17@gmail.com>
2024-01-22 22:07:21 +0700

    Fixed Button BLYNK

--- IoT_saja/IoT_saja.ino
+++ IoT_saja/IoT_saja.ino
@@ -4,34 +4,36 @@
 
 #include <Blynk.h>
 #define BLYNK_PRINT Serial
+#define BUTTON_PIN2 V2
 
 #include <WiFi.h>
 #include <WiFiClient.h>
 #include <BlynkSimpleEsp32.h>
 
-#include <DHT.h>
 #include <LiquidCrystal_I2C.h>
 
 #include <Wire.h>
 #include <Adafruit_MLX90614.h>
 
-#define DHT_SENSOR_PIN 4
-#define DHT_SENSOR_TYPE DHT22
 #define KIPAS_PIN 17
 #define EXOS_PIN 5
 #define HEATER_PIN 16
 #define BUTTON_PIN 18
 
-DHT dht_sensor(DHT_SENSOR_PIN, DHT_SENSOR_TYPE);
 LiquidCrystal_I2C lcd(0x27, 20, 4);  // Alamat I2C untuk modul 20x4
 Adafruit_MLX90614 mlx = Adafruit_MLX90614();
 
 int isFirstPress = 0;
+int buttonState = 0;
 bool lastButtonState = HIGH;
 bool currentButtonState;
-
+bool isDeviceOn = false;
 char ssid[] = "Galaxy A727885";
 char pass[] = "ginnamon";
+unsigned long startTime = 0;
+BLYNK_WRITE(BUTTON_PIN2){
+  buttonState = param.asInt();
+}
 
 void setup() {
   Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
@@ -41,7 +43,6 @@ void setup() {
   pinMode(BUTTON_PIN, INPUT_PULLUP); // Using INPUT_PULLUP to enable internal pull-up resistor
   lcd.init();
   Serial.begin(115200);
-  dht_sensor.begin();
   mlx.begin();
   
   lcd.begin(20, 4);        // Inisialisasi LCD 20x4
@@ -58,6 +59,7 @@ void setup() {
     delay(1000);
     Serial.println("Connecting to WiFi");
   }
+  startTime = millis();
 }
 
 void loop() {
@@ -69,38 +71,80 @@ void loop() {
   Serial.print("*C\tObject = ");
   Serial.print(object);
   Serial.println("*C");
+  Serial.println(isFirstPress);
     
-  lcd.setCursor(2, 2);
-  lcd.print("RUANGAN = ");
-  lcd.print(ambient);
-  lcd.print("C");
-  lcd.setCursor(2, 3);
-  lcd.print("PEMANAS = ");
+  lcd.setCursor(3, 2);
+  lcd.print("SUHU = ");
   lcd.print(object);
-  lcd.print("C");
+  lcd.print(" C");
   currentButtonState = digitalRead(BUTTON_PIN);
 
-if (currentButtonState == LOW && lastButtonState == HIGH) {
+  if (currentButtonState == LOW && lastButtonState == HIGH) {
     isFirstPress++;
-    if (isFirstPress == 1) {
-      lcd.setCursor(9, 1);
-      lcd.print("ON");
-      Serial.println("1");
-      digitalWrite(KIPAS_PIN, LOW);
-      digitalWrite(EXOS_PIN, LOW);
-      digitalWrite(HEATER_PIN, LOW);
-    } else {
-      Serial.println("0");
-      lcd.setCursor(8, 1);
-      lcd.print("OFF!");
-      digitalWrite(KIPAS_PIN, HIGH);
-      digitalWrite(EXOS_PIN, HIGH);
-      digitalWrite(HEATER_PIN, HIGH);
-      isFirstPress = 0;
+  } 
+
+  if (isFirstPress == 1 || buttonState == HIGH){
+    isDeviceOn = true;
+  }
+  else if (isFirstPress == 2 || buttonState == LOW){
+    isDeviceOn = false;
+    isFirstPress = 0;
+  }
+
+  if (isDeviceOn) {
+    digitalWrite(KIPAS_PIN, LOW);
+    digitalWrite(EXOS_PIN, LOW);
+    digitalWrite(HEATER_PIN, LOW);
+  } else {
+    digitalWrite(KIPAS_PIN, HIGH);
+    digitalWrite(EXOS_PIN, HIGH);
+    digitalWrite(HEATER_PIN, HIGH);
     }
+  
+  if (isDeviceOn){
+    unsigned long currentTime = millis();
+    unsigned long elapsedTime = currentTime - startTime;
+
+    int hours = elapsedTime / 3600000;
+    int minutes = (elapsedTime % 3600000) / 60000;
+    int seconds = ((elapsedTime % 3600000) % 60000) / 1000;
+    lcd.clear();
+    lcd.setCursor(7, 0);     // Set kursor ke baris 1, kolom 1
+    lcd.print("SoLveD");
+    lcd.setCursor(9, 1);
+    lcd.print("ON");
+    lcd.setCursor(3, 2);
+    lcd.print("SUHU = ");
+    lcd.print(object);
+    lcd.print(" C");
+    lcd.setCursor(2, 3);
+    lcd.print("WAKTU = ");
+    lcd.print(hours);
+    lcd.print(":");
+    lcd.print(minutes);
+    lcd.print(":");
+    lcd.print(seconds);
+  }
+  else {
+    lcd.clear();
+    startTime = millis();
+    lcd.setCursor(7, 0);     // Set kursor ke baris 1, kolom 1
+    lcd.print("SoLveD");
+    lcd.setCursor(8, 1);
+    lcd.print("OFF!");
+    lcd.setCursor(3, 2);
+    lcd.print("SUHU = ");
+    lcd.print(object);
+    lcd.print(" C");
+    lcd.setCursor(2, 3);
+    lcd.print("WAKTU = 00:00:00");
   }
 
   lastButtonState = currentButtonState;
+
+  Blynk.virtualWrite(V0, object);
+  Blynk.virtualWrite(V1, ambient);
+  // Blynk.virtualWrite(V2, isFirstPressBlynk);
 
   delay(1000);
   Blynk.run();
